9ab07715b05054f2c20ecfd95dcaf4e11f34c544
Abdul Salaam <salaamazimi21@gmail.com>
2025-06-25 20:29:00 +0430

    Update CourseCard data
     update change image and link

--- src/layout/CourseCard.jsx
+++ src/layout/CourseCard.jsx
@@ -4,11 +4,15 @@ import PropTypes from "prop-types";
 function CourseCard( { imageSrc, imageTitle, title, description, link} ) {
 
     return (
-        <div className="relative col-span-full flex flex-col items-start bg-gray-100 mt-8 p-12 rounded-lg">
-            <img src={imageSrc} alt={imageTitle} />
+        <div className="relative flex flex-col bg-gray-100 mt-8 p-12 rounded-lg w-full h-full">
+            <img src={imageSrc} alt={imageTitle} className="w-24 h-24" />
             <h2 className="text-gray-800 font-semibold text-xl mt-12 pr-10">{title}</h2>
-            <p className="mt-2 mb-6 text-gray-500 max-w-[600px]">{description}</p>
-            <Link to={link} className="text-lg hover:bg-white pt-2 pb-2 pr-4 pl-4 rounded-full border border-gray-300">Visit course</Link>
+            <div className="flex flex-col flex-grow justify-between mt-2"> 
+                <p className="mt-2 mb-6 text-gray-500 max-w-[600px]">{description}</p>
+                <div>
+                    <Link to={link} className="text-lg hover:bg-white pt-2 pb-2 pr-4 pl-4 rounded-full border border-gray-300">Visit course</Link>
+                </div>
+            </div>
         </div>
     )
 }
