16ef2073c6a70cc4ab8b64228adfda74b8c9844b
Abdul Salaam <salaamazimi21@gmail.com>
2025-06-28 00:23:47 +0430

    update course card data
    add new card and change some data of the cards

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -10,113 +10,114 @@ function CourseList() {
       imageSrc: Remix,
       title: 'Advanced Remix',
       description:
-        "Remix is a terrific tool for building simple websites and even better for building complex web applications. Remix solves many problems...",
+        "Remix is a terrific tool for building simple websites and even better for building complex web applications. Remix solves many problems in modern web development. You don't even think about server cache management or global CSS namespace clashes. It's not that Remix has APIs to avoid these problems; they simply don't exist when you're using Remix!",
       link: '#'
     },
     {
-      imageSrc: Remix,
+      imageSrc: "",
       title: 'Remix Fundamentals',
       description:
-        "Remix is a fullstack web framework that enables you to deliver a fast, slick, and resilient user experience...",
+        "Remix is a fullstack web framework that enables you to deliver a fast, slick, and resilient user experience. With Remix, you can build both static websites and dynamic web apps (requiring user data) while embracing the web platform's standard APIs along the way! Ready to build web apps faster?",
       link: '#'
     },
     {
-      imageSrc: Remix,
+      imageSrc: "",
       title: 'Up and Running with Remix',
       description:
-        "Jump in feet first and learn the most productive way to build a web application with Remix...",
+        "Jump in feet first and learn the most productive way to build a web application with the web framework that offers the best UX and DX the web has to offer.",
+      link: '#'
+    },
+    {
+      imageSrc: "",
+      title: 'The Beginners Guide to React',
+      description:
+        "This course is for React newbies and anyone looking to build a solid foundation. It's designed to teach you everything you need to start building web applications in React right away.",
+      link: '#'
+    },
+    {
+      imageSrc: "",
+      title: 'Use Suspense to Simplify Your Async UI',
+      description:
+        "In this course, I teach how Suspense works under the hood, preparing you for the future of asynchronous state management in React.",
+      link: '#'
+    },
+    {
+      imageSrc: "",
+      title: 'Simplify React Apps with React Hooks',
+      description:
+        "In this course, I will take a modern React codebase that uses classes and refactor the entire thing to use function components as much as possible. We'll look at state, side effects, async code, caching, and more!",
+      link: '#'
+    },
+    {
+      imageSrc: "",
+      title: 'Advanced React Component Patterns',
+      description:
+        "Once you've nailed the fundamentals of React, that's when things get really fun. This course teaches you advanced patterns in React that you can use to make components that are simple, flexible, and enjoyable to work with.",
+      link: '#'
+    },
+    {
+      imageSrc: "",
+      title: 'JavaScript Testing Practices and Principles',
+      description:
+        "Learn the principles and best practices for writing maintainable test applications to catch errors before your product reaches the end user!",
+      link: '#'
+    },
+    {
+      imageSrc: "",
+      title: 'Testing React Applications',
+      description:
+        "Fix errors before your app reaches the end user by writing maintainable unit test & integration tests for your React applications!",
+      link: '#'
+    },
+    {
+      imageSrc: "",
+      title: 'Code Transformation & Linting with ASTs',
+      description:
+        "Learn to use Abstract Syntax Trees (ASTs) to make stylistic code changes, reveal logical problems, and prevent bugs from entering your codebase.",
+      link: '#'
+    },
+    {
+      imageSrc: "",
+      title: 'How to Write an Open Source JavaScript Library',
+      description:
+        "From Github and npm, to releasing beta versions, semantic versioning, code coverage, continuous integration, and providing your library with a solid set of unit tests, there are a ton of things to learn. This series will guide you through a set of steps to publish a JavaScript open source library.",
+      link: '#'
+    },
+    {
+      imageSrc: "",
+      title: 'How to Contribute to an Open Source Project on GitHub',
+      description:
+        "“Feel free to submit a PR!” - words often found in GitHub issues, but met with confusion and fear by many. Getting started with contributing open source is not always straightforward and can be tricky. With this series, you'll be equipped with the the tools, knowledge, and understanding you need to be productive and contribute to the wonderful world of open source projects.",
       link: '#'
     }
 ];
 
   const courseGroups = [];
   for (let i = 0; i < courses.length; i += 3) {
-    courseGroups.push(courses.slice())
+    courseGroups.push(courses.slice( i , i + 3))
+  }
+
+  const renderGroup = (group, index) => {
+    if (group.length < 3) return null;
+
+    return (
+      <div key={index} className="grid grid-cols-1 gap-8 lg:grid-cols-12">
+        <div className="lg:col-span-12">
+          <CourseCard {...group[0]} />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:col-span-12 gap-6">
+          <CourseCard {...group[1]} />
+          <CourseCard {...group[2]} />
+        </div>
+      </div>
+    );
   }
 
     return (
-        <div className="grid grid-cols-4 gap-x-4 md:grid-cols-8 lg:grid-cols-12 lg:gap-x-6">
-            {courses.map((course , index) => (
-            <CourseCard key={index} {...course} />
-    ))}
-        </div>
-
+      <div className="space-y-12">
+        {courseGroups.map((group, index) => renderGroup(group, index))}
+      </div>
     )
 }
-
-
-
-
-// function CourseList() {
-//   const allCourses = [
-//     {
-//       imageSrc: Remix,
-//       title: 'Advanced Remix',
-//       description: 'Remix is a terrific tool for building complex web applications...',
-//       link: '#',
-//     },
-//     {
-//       imageSrc: Remix,
-//       title: 'Remix Fundamentals',
-//       description: 'Remix is a fullstack web framework...',
-//       link: '#',
-//     },
-//     {
-//       imageSrc: Remix,
-//       title: 'Up and Running with Remix',
-//       description: 'Jump in feet first... ',
-//       link: '#',
-//     },
-//     {
-//       imageSrc: Remix,
-//       title: 'Testing with Remix',
-//       description: 'Learn how to test Remix apps...',
-//       link: '#',
-//     },
-//     {
-//       imageSrc: Remix,
-//       title: 'Deploying Remix',
-//       description: 'Steps to deploy your Remix app...',
-//       link: '#',
-//     },
-//     {
-//       imageSrc: Remix,
-//       title: 'Remix and SEO',
-//       description: 'Optimize Remix apps for SEO...',
-//       link: '#',
-//     },
-//   ];
-
-//   // Group into sets of 3
-//   const courseGroups = [];
-//   for (let i = 0; i < allCourses.length; i += 3) {
-//     courseGroups.push(allCourses.slice(i, i + 3));
-//   }
-
-//   // Render 1 card + 2 side-by-side cards
-//   const renderGroup = (group, index) => {
-//     if (group.length < 3) return null;
-
-//     return (
-//       <div key={index} className="grid grid-cols-1 gap-8 lg:grid-cols-12">
-//         <div className="lg:col-span-12">
-//           <CourseCard {...group[0]} />
-//         </div>
-//         <div className="grid grid-cols-1 md:grid-cols-2 lg:col-span-12 gap-6">
-//           <CourseCard {...group[1]} />
-//           <CourseCard {...group[2]} />
-//         </div>
-//       </div>
-//     );
-//   };
-
-//   return (
-//     <div className="space-y-12">
-//       {courseGroups.map((group, index) => renderGroup(group, index))}
-//     </div>
-//   );
-// }
-
-
 
 export default CourseList;
